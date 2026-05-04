d61284d7ccecf7dc2fbf2dafdbf7902b944750de
felipeB92 <felipe_B92@hotmail.com>
2026-05-04 14:45:43 -0300

    implementação de test foco Abstração Mini game Poket Monsters

--- Testes/Mini_Game_poketMonsters/Batalha.py
+++ Testes/Mini_Game_poketMonsters/Batalha.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from random import randint
+from rich import print
+from rich.panel import Panel
+from rich.columns import Columns
+from time import sleep
 
 # TIPOS
 #1 = Fogo
@@ -13,39 +17,59 @@ class pokemon(ABC):
 
     def Ataque(self,alvo):
         self.alvo = alvo
-        print(f'{self.nome} Atacou com {self.cor}{self.ataques[randint(0,3)]}\033[m')
+        print(f'{18*' '}[bold]INCIANDO BATALHA')
+        self.painel()
+        sleep(1)
+        print()
+        sleep(1)
+        print(f'{self.nome} Atacou com {self.cor}{self.ataques[randint(0,3)]}')
         self.defesa()
 
     def defesa(self):
-        self.dano = 100/(randint(1,5))
-        if self.nivel >= self.alvo.nivel:
-            self.dano += (abs(self.alvo.nivel - self.nivel))/2
-        else:
-            self.dano -= (abs(self.alvo.nivel - self.nivel))/2
+        self.dano = 10*(randint(5,6))
+        if self.nivel > self.alvo.nivel:
+            self.dano += (abs(self.nivel - self.alvo.nivel)/2)
+        if self.nivel < self.alvo.nivel:
+            self.dano -= (abs(self.alvo.nivel - self.nivel)/2)
 
         if self.tipo == 1 and self.alvo.tipo == 2 or self.tipo == 2 and self.alvo.tipo == 3 or self.tipo == 3 and self.alvo.tipo == 1:
-            print('\033[36;1mnão é ,muito efetivo\033[m')
-            self.dano -= (self.dano/100)*(randint(10,20))
+            sleep(1)
+            print(f'[orange1]não é ,muito efetivo[/]')
+            self.dano -= abs(self.dano/randint(2,3))
         else:
-            print('\033[33;1mmuito efetivo\033[m')
-            self.dano += (self.dano / 100) * (randint(10, 20))
+            sleep(1)
+            print(f'[bright_green]muito efetivo[/]')
+            self.dano += abs(self.dano/randint(2,3))
         if self.dano <= 0:
+            sleep(1)
             print(f'{self.nome} errou o ataque {self.alvo.nome} não recebeu dano')
         else:
+            sleep(1)
             print(f'{self.alvo.nome} recebeu {self.dano:.0f} de dano')
             self.alvo.vida -= self.dano
         if self.alvo.vida <= 0:
+            sleep(1)
             print(f'{self.alvo.nome} ficou sem HP e desmaiou')
+            print(f'{self.cor}{self.nome} Venceu')
         else:
+            sleep(1)
             print(f'{self.alvo.nome} Ficou com {self.alvo.vida:.0f} de HP')
 
+    def painel(self):
+        self.PAINEL = (Panel(f'LEVEL= {self.nivel}\nTIPO={self.tipoe}\nHP= {self.vida:.0f}\nATAQUES=\n{", ".join(self.ataques)}', title=f'{
+            self.nome}',width=25))
+        self.PAINEL2 = (Panel(f'LEVEL= {self.alvo.nivel}\nTIPO={self.alvo.tipoe}\nHP= {self.alvo.vida:.0f}\nATAQUES=\n{", ".join(self.alvo.ataques)}', title=f'{
+            self.alvo.nome}',width=25))
+        sleep(0.5)
+        print(Columns([self.PAINEL,self.PAINEL2]))
 
 class Charmander(pokemon):
     def __init__(self,nivel,nome='Charmander'):
         super().__init__(nome)
         self.nivel = nivel
         self.tipo = 1
-        self.cor = '\033[31;1m'
+        self.tipoe = 'Fogo'
+        self.cor = '[red]'
         self.ataques = ['brasa','lança chamas','giro de fogo','investida de calor']
 
 class Squirtle(pokemon):
@@ -53,16 +77,41 @@ class Squirtle(pokemon):
         super().__init__(nome)
         self.nivel = nivel
         self.tipo = 2
-        self.cor = '\033[34;1m'
+        self.tipoe = 'Agua'
+        self.cor = '[blue]'
         self.ataques = ['jato de agua','bolhas','hidrobomba','cauda de gelo']
 
 class Bulbasaur (pokemon):
     def __init__(self,nivel,nome='Bulbasaur'):
         super().__init__(nome)
         self.nivel = nivel
-        self.cor = '\033[32;1m'
+        self.cor = '[green]'
         self.tipo = 3
+        self.tipoe = 'Grama'
         self.ataques = ['Chicote de cipo','pó venenosos','Folha de Navalha','Raio Solar']
+
+def umatres(N):
+    t= 0
+    while True:
+        try:
+            t = int(input(f'{N}'))
+            if t ==1 or t ==2 or t ==3:
+                break
+            else:
+                print('opção invalida')
+        except:
+            print('invalido')
+    return t
+
+def eint(N):
+    while True:
+        try:
+            t = int(input(f'{N}'))
+            return t
+        except:
+            print('invalido')
+
+
 
 
 
--- Testes/Mini_Game_poketMonsters/__main__.py
+++ Testes/Mini_Game_poketMonsters/__main__.py
@@ -1,9 +1,44 @@
+from pygments.lexers import j
+
 from Batalha import *
+from rich import print
 
-p1 = Charmander(50)
-p2 = Squirtle(25)
-p3 = Bulbasaur(12)
+print(f'[1][green]bulbasaur')
+print(f'[2][blue]squirtle')
+print(f'[3][red]charmander')
 
-p1.Ataque(p3)
-p2.Ataque(p1)
-p3.Ataque(p2)
+J= umatres('Escolha seu pokemon: ')
+n= eint('Qual nivel do seu pokemon: ')
+
+i= randint(1,3)
+while J == i:
+    i = randint(1,3)
+
+n2 =randint(10,100)
+if J == 3:
+    p1 = Charmander(n)
+if J == 2:
+    p1 = Squirtle(n)
+if J == 1:
+    p1 = Bulbasaur(n)
+
+if i == 3:
+    p2 = Charmander(n2)
+if i == 2:
+    p2 = Squirtle(n2)
+if i == 1:
+    p2 = Bulbasaur(n2)
+
+while True:
+    if p1.vida <= 0:
+        break
+    else:
+        p1.Ataque(p2)
+    if p2.vida <=0:
+        break
+    else:
+        p2.Ataque(p1)
+
+print('fim da batalha')
+
+
